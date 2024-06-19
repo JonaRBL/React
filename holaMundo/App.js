d95cae099f7284113340a3919534ef62d10360a4
JonaRBL <121039246@upq.edu.mx>
2024-06-18 20:14:49 -0600

    crear mi propio componente

--- holaMundo/App.js
+++ holaMundo/App.js
@@ -1,10 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import React, {useState} from 'react';
+
+const Texto=(props)=>{
+  const {children} = props
+  return(
+    <Text> {children} </Text>
+  )
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hola mundo ReactNative</Text>
+      <Texto >uno</Texto>
+      <Texto >dos</Texto>
+      {/* <Texto /> */}
+      {/* <Text>Hola mundo ReactNative</Text> */}
+      <Button title='Saludar' color='green'></Button>
       <StatusBar style="auto" />
     </View>
   );
